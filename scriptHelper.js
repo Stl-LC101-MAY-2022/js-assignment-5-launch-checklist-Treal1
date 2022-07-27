9896d790cb94370212e25416a83ce7dbd2f9815e
Torrie Real <torijames1997@gmail.com>
2022-07-27 13:39:20 -0500

    Added shuttle requirements for pilot names, the fuel level, and the launch status color change

--- scriptHelper.js
+++ scriptHelper.js
@@ -40,6 +40,45 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    let cargoStatus = document.getElementById("cargoStatus");
    let launchStatus = document.getElementById("launchStatus");
    let liftOff = true;
+
+   if(validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+    alert("All fields are required");
+    } else if ((validateInput(fuelLevel) === "Not a number") || validateInput(cargoLevel) === "Not a number") {
+    alert("Values must contain numbers.");
+    } else if ((validateInput(pilot) === "Is a number") || validateInput(copilot) === "Is a number") {
+    alert("Values must contain letters.")    
+    }
+
+    function weAreReady() {
+        list.style.visibility = "visible";
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Copilot ${copilot} is ready for launch`;
+    };
+    
+   if (fuelLevel < 10000) {
+        liftOff = false
+        fuelStatus.innerHTML = "There is not enough fuel for the journey";
+   } else {
+       fuelStatus.innerHTML = "There is enough fuel for the journey";   
+   }
+
+   if (cargoLevel > 10000) {
+        liftOff = false;
+        cargoStatus.innerHTML = "There is too much mass for the shuttle to take off";
+   } else {
+        cargoStatus.innerHTML = "Mass is low enough for the shuttle to take off";
+   };
+
+   if (liftOff) {
+    launchStatus.innerHTML =  "Shuttle is ready for launch";
+    launchStatus.style.color = "green";
+    weAreReady();
+   } else {
+    launchStatus.innerHTML =  "Shuttle is not ready for launch";
+    launchStatus.style.color = "red";
+    weAreReady();
+   }
+};
 }
 
 async function myFetch() {
